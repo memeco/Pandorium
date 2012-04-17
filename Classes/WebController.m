612a850d6133087621b98011b2c60f75a1adc15b
Gaurav Khanna <gauravk92@gmail.com>
2012-04-17 19:41:39 -0400

    fixed bug with signin/home page

--- Classes/WebController.m
+++ Classes/WebController.m
@@ -67,7 +67,7 @@
         webview.shouldUpdateWhileOffscreen = TRUE;
         webview.customUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.53.11 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10";
         //webview.customUserAgent = @"Mozilla/5.0 (Windows NT 6.1; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1";
-        webview.mainFrameURL = @"https://www.pandora.com/#/account/sign-in";
+        webview.mainFrameURL = @"https://www.pandora.com";
         
         WebPreferences* prefs = webview.preferences;
         [prefs _setLocalStorageDatabasePath:[PrefController localStoragePath]];
@@ -153,7 +153,7 @@
     [self setupWindowScrolling];
     //DLogObject(request.URL.description);
     
-    /*if ([request.URL.description isEqualToString:@"https://www.pandora.com/#/account/sign-in"]) {
+    /*if ([request.URL.description isEqualToString:@"https://www.pandora.com"]) {
         return @"InitialPage";
     }
     
