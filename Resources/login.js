5d5a93c81508c054bd82ef6d0a0af202a819ff84
Gaurav Khanna <gauravk92@gmail.com>
2012-05-17 15:28:22 -0400

    some cleanup to extraneous files

--- Resources/login.js
+++
@@ -1,6 +0,0 @@
-if(document.getElementById('loginStyle') === null) {
-    var style = document.createElement('style');
-    style.setAttribute('id','loginStyle');
-    style.innerHTML = '%%CSS%%';
-    document.body.appendChild(style);
-}
